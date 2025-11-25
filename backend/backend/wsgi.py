36c4b5498f37377ff95caaa01739d15c87bcb58d
Hariette <sepillohariette@gmail.com>
2025-11-25 21:42:11 +0800

    te barang ka barang ka babalik sayo yan

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add the Django project directory to the Python path
+path = '/home/hayett/BorrowLink/backend'
+if path not in sys.path:
+    sys.path.append(path)
 
+# Set the settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
